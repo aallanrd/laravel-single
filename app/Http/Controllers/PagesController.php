e6e5d154653d151e9bc7177cb7f2e9d1bdc1dcb9
Allan Rojas <allanjose91@gmail.com>
2019-02-20 08:14:38 -0600

    Controller Class

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function home()
+    {
+
+      	// $tasks = [
+      	// 	'Go to the market',
+      	// 	'Go to the store',
+      	// 	'Go to work'
+      	// ];
+
+          return view('welcome',[
+          	'foo' => 'bar'
+          ]);
+    }
+
+    public function about(){
+      return view('about');
+    }
+
+    public function contact(){
+      return view('contact');
+    }
 }
